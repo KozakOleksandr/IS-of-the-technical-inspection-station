b839a43bad930b99a2a22e33af6c5ab6a02d9b62
KozakOleksandr <127938868+KozakOleksandr@users.noreply.github.com>
2023-12-16 03:13:21 +0200

    Half of the project

---
+++ admin/home.php
@@ -0,0 +1,80 @@
+<h1 class="text-light">Welcome to <?php echo $_settings->info('name') ?></h1>
+<hr class="border-light">
+<div class="row">
+          <div class="col-12 col-sm-6 col-md-3">
+            <div class="info-box">
+              <span class="info-box-icon bg-light elevation-1"><i class="fas fa-th-list"></i></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text">Кількість марок </span>
+                <span class="info-box-number">
+                  <?php 
+                    $inv = $conn->query("SELECT count(id) as total FROM categories ")->fetch_assoc()['total'];
+                    echo number_format($inv);
+                  ?>
+                  <?php ?>
+                </span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+          <div class="col-12 col-sm-6 col-md-3">
+            <div class="shadow info-box mb-3">
+              <span class="info-box-icon bg-info elevation-1"><i class="fas fa-users-cog"></i></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text">Механіки</span>
+                <span class="info-box-number">
+                  <?php 
+                    $mechanics = $conn->query("SELECT COUNT(*) as total FROM  `mechanics_list`")->fetch_assoc()['total'];
+                    echo number_format($mechanics);
+                  ?>
+                </span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <!-- /.col -->
+
+          <!-- fix for small devices only -->
+          <div class="clearfix hidden-md-up"></div>
+
+          <div class="col-12 col-sm-6 col-md-3">
+            <div class="shadow info-box mb-3">
+              <span class="info-box-icon bg-success elevation-1"><i class="fas fa-th-list"></i></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text">Послуги</span>
+                <span class="info-box-number">
+                <?php
+                $result = $conn->query("SELECT COUNT(*) as total FROM `service_list`");
+                $total = $result->fetch_assoc()['total'];
+                echo number_format($total);
+                ?>
+                </span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+          <div class="col-12 col-sm-6 col-md-3">
+            <div class="shadow info-box mb-3">
+              <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-file-invoice"></i></span>
+
+              <div class="info-box-content">
+                <span class="info-box-text">Виконанні записи</span>
+                <span class="info-box-number">
+                <?php 
+                    $services = $conn->query("SELECT COUNT(*) as total FROM `service_requests` where status = 3 ")->fetch_assoc()['total'];
+                    echo number_format($services);
+                  ?>
+                </span>
+              </div>
+              <!-- /.info-box-content -->
+            </div>
+            <!-- /.info-box -->
+          </div>
+        </div>
---
+++ success_msg.php
@@ -0,0 +1,13 @@
+<style>
+    #uni_modal .modal-footer,#uni_modal .modal-header{
+        display:none
+    }
+</style>
+<div class="container-fluid">
+    <p>Ваш Запис успішно надіслано. Оператор СТО зв'яжеться з вами, як тільки побачить ваш запис. Дякуєм за довіру!</p>
+    <div class="w-100 d-flex justify-content-end mx-2">
+        <div class="col-auto">
+            <button class="btn btn-dark btn-sm rounded-0" type="button" data-dismiss="modal">Закрити</button>
+        </div>
+    </div>
+</div>
